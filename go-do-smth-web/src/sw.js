27564ace62a449031bcb532442e51d2fe7127bbb
Alexander Zwisler <alex-z-@web.de>
2020-09-28 20:38:45 +0200

    sw

--- go-do-smth-web/src/sw.js
+++ go-do-smth-web/src/sw.js
@@ -66,7 +66,8 @@ self.addEventListener('fetch', (event) => {
   // Skip cross-origin requests, like those for Google Analytics.
   if (
     event.request.url.startsWith(self.location.origin) &&
-    event.request.method === 'GET'
+    event.request.method === 'GET' &&
+    !event.request.url.containts('/api/')
   ) {
     event.respondWith(
       caches.match(event.request).then((cachedResponse) => {
